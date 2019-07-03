98380079448a0792d23e49357e51d8d2a91f211a
angelsollaperez <angelsollaperez@gmail.com>
2019-07-03 22:43:19 +0200

    initial commit

---
+++ dockertest/README.md
@@ -0,0 +1,4 @@
+# dockertest
+
+TODO: Enter the cookbook description here.
+
---
+++ dockertest/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: dockertest
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
